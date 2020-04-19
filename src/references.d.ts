4f49b40b1bc8d49422e4a04dc509f1c0156d88a2
Paul McGougan <paul@plmservices.com.au>
2020-04-20 08:38:38 +1000

    Update references

--- src/references.d.ts
+++ src/references.d.ts
@@ -2,7 +2,7 @@
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
 
 /// <reference path="./platforms/ios/typings/objc!Particle_SDK-1.0.4.d.ts" />
-/// <reference path="./platforms/ios/typings/objc!ParticleSetup-1.0.2.d.ts" />
+/// <reference path="./platforms/ios/typings/objc!ParticleSetup-1.0.3.d.ts" />
 
 /// <reference path="./platforms/android/typings/cloudsdk-1.0.0.d.ts" />
 /// <reference path="./platforms/android/typings/devicesetup-0.6.3.d.ts" />
